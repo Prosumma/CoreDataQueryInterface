39388f397302c0eb3b436db09d65220b94e75400
Gregory Higley <code@revolucent.net>
2015-04-10 22:57:58 -0400

    Initial Commit

---
+++ CoreDataQueryInterface/CoreDataQueryInterface.h
@@ -0,0 +1,19 @@
+//
+//  CoreDataQueryInterface.h
+//  CoreDataQueryInterface
+//
+//  Created by Gregory Higley on 4/10/15.
+//  Copyright (c) 2015 Prosumma LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CoreDataQueryInterface.
+FOUNDATION_EXPORT double CoreDataQueryInterfaceVersionNumber;
+
+//! Project version string for CoreDataQueryInterface.
+FOUNDATION_EXPORT const unsigned char CoreDataQueryInterfaceVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CoreDataQueryInterface/PublicHeader.h>
+
+
