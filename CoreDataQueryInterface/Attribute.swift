d7defd6fb7227bf95c21aa38a67b163f184ed737
Gregory Higley <code@revolucent.net>
2015-06-04 19:48:53 -0400

    Better operators.
    Boy did I fuck that up. New release, coming up!

--- CoreDataQueryInterface/Attribute.swift
+++ CoreDataQueryInterface/Attribute.swift
@@ -44,82 +44,314 @@ public prefix func !(predicate: NSPredicate) -> NSPredicate {
     return NSCompoundPredicate.notPredicateWithSubpredicate(predicate)
 }
 
-public func predicate<A: AttributeType>(lhs: A, op: String, rhs: NSObject) -> NSPredicate {
-    return NSPredicate(format: "%K \(op) %@", lhs.description, rhs)
+public func predicate<A: AttributeType, T>(lhs: A, op: String, rhs: T?, convert: T -> NSObject) -> NSPredicate {
+    if let rhs = rhs {
+        return NSPredicate(format: "%K \(op) %@", lhs.description, convert(rhs))
+    } else {
+        return NSPredicate(format: "%K \(op) NIL", lhs.description)
+    }
 }
 
 public func predicate<A: AttributeType, B: AttributeType>(lhs: A, op: String, rhs: B) -> NSPredicate {
     return NSPredicate(format: "%K \(op) %K", lhs.description, rhs.description)
 }
 
-public func ==<A: AttributeType>(lhs: A, rhs: NSNumber) -> NSPredicate {
-    return predicate(lhs, "==", rhs)
+/* == */
+
+public func ==<A: AttributeType>(lhs: A, rhs: NSObject?) -> NSPredicate {
+    return predicate(lhs, "==", rhs) {$0}
 }
 
-public func !=<A: AttributeType>(lhs: A, rhs: NSNumber) -> NSPredicate {
-    return predicate(lhs, "!=", rhs)
+public func ==<A: AttributeType>(lhs: A, rhs: String?) -> NSPredicate {
+    return predicate(lhs, "==", rhs) {$0 as NSString}
 }
 
-public func ><A: AttributeType>(lhs: A, rhs: NSNumber) -> NSPredicate {
-    return predicate(lhs, ">", rhs)
+public func ==<A: AttributeType>(lhs: A, rhs: UInt8?) -> NSPredicate {
+    return predicate(lhs, "==", rhs) {NSNumber(unsignedChar: $0)}
 }
 
-public func >=<A: AttributeType>(lhs: A, rhs: NSNumber) -> NSPredicate {
-    return predicate(lhs, ">=", rhs)
+public func ==<A: AttributeType>(lhs: A, rhs: UInt16?) -> NSPredicate {
+    return predicate(lhs, "==", rhs) {NSNumber(unsignedShort: $0)}
 }
 
-public func <<A: AttributeType>(lhs: A, rhs: NSNumber) -> NSPredicate {
-    return predicate(lhs, "<", rhs)
+public func ==<A: AttributeType>(lhs: A, rhs: Int16?) -> NSPredicate {
+    return predicate(lhs, "==", rhs) {NSNumber(short: $0)}
 }
 
-public func <=<A: AttributeType>(lhs: A, rhs: NSNumber) -> NSPredicate {
-    return predicate(lhs, "<=", rhs)
+public func ==<A: AttributeType>(lhs: A, rhs: UInt32?) -> NSPredicate {
+    return predicate(lhs, "==", rhs) {NSNumber(unsignedInt: $0)}
 }
 
-public func ==<A: AttributeType>(lhs: A, rhs: NSObject) -> NSPredicate {
-    return predicate(lhs, "==", rhs)
+public func ==<A: AttributeType>(lhs: A, rhs: Int32?) -> NSPredicate {
+    return predicate(lhs, "==", rhs) {NSNumber(int: $0)}
 }
 
-public func !=<A: AttributeType>(lhs: A, rhs: NSObject) -> NSPredicate {
-    return predicate(lhs, "!=", rhs)
+public func ==<A: AttributeType>(lhs: A, rhs: UInt64?) -> NSPredicate {
+    return predicate(lhs, "==", rhs) {NSNumber(unsignedLongLong: $0)}
 }
 
-public func ><A: AttributeType>(lhs: A, rhs: NSObject) -> NSPredicate {
-    return predicate(lhs, ">", rhs)
+public func ==<A: AttributeType>(lhs: A, rhs: Int64?) -> NSPredicate {
+    return predicate(lhs, "==", rhs) {NSNumber(longLong: $0)}
 }
 
-public func >=<A: AttributeType>(lhs: A, rhs: NSObject) -> NSPredicate {
-    return predicate(lhs, ">=", rhs)
+public func ==<A: AttributeType>(lhs: A, rhs: Int?) -> NSPredicate {
+    return predicate(lhs, "==", rhs) {NSNumber(integer: $0)}
 }
 
-public func <<A: AttributeType>(lhs: A, rhs: NSObject) -> NSPredicate {
-    return predicate(lhs, "<", rhs)
+public func ==<A: AttributeType>(lhs: A, rhs: Float?) -> NSPredicate {
+    return predicate(lhs, "==", rhs) {NSNumber(float: $0)}
 }
 
-public func <=<A: AttributeType>(lhs: A, rhs: NSObject) -> NSPredicate {
-    return predicate(lhs, "<=", rhs)
+public func ==<A: AttributeType>(lhs: A, rhs: Double?) -> NSPredicate {
+    return predicate(lhs, "==", rhs) {NSNumber(double: $0)}
 }
 
-public func ==<A: AttributeType, B: AttributeType>(lhs: A, rhs: B) -> NSPredicate {
-    return predicate(lhs, "==", rhs)
+/* != */
+
+public func !=<A: AttributeType>(lhs: A, rhs: NSObject?) -> NSPredicate {
+    return predicate(lhs, "==", rhs) {$0}
 }
 
-public func !=<A: AttributeType, B: AttributeType>(lhs: A, rhs: B) -> NSPredicate {
-    return predicate(lhs, "!=", rhs)
+public func !=<A: AttributeType>(lhs: A, rhs: String?) -> NSPredicate {
+    return predicate(lhs, "==", rhs) {$0 as NSString}
 }
 
-public func ><A: AttributeType, B: AttributeType>(lhs: A, rhs: B) -> NSPredicate {
-    return predicate(lhs, ">", rhs)
+public func !=<A: AttributeType>(lhs: A, rhs: UInt8?) -> NSPredicate {
+    return predicate(lhs, "==", rhs) {NSNumber(unsignedChar: $0)}
 }
 
-public func >=<A: AttributeType, B: AttributeType>(lhs: A, rhs: B) -> NSPredicate {
-    return predicate(lhs, ">=", rhs)
+public func !=<A: AttributeType>(lhs: A, rhs: UInt16?) -> NSPredicate {
+    return predicate(lhs, "==", rhs) {NSNumber(unsignedShort: $0)}
 }
 
-public func <<A: AttributeType, B: AttributeType>(lhs: A, rhs: B) -> NSPredicate {
-    return predicate(lhs, "<", rhs)
+public func !=<A: AttributeType>(lhs: A, rhs: Int16?) -> NSPredicate {
+    return predicate(lhs, "==", rhs) {NSNumber(short: $0)}
 }
 
-public func <=<A: AttributeType, B: AttributeType>(lhs: A, rhs: B) -> NSPredicate {
-    return predicate(lhs, "<=", rhs)
+public func !=<A: AttributeType>(lhs: A, rhs: UInt32?) -> NSPredicate {
+    return predicate(lhs, "==", rhs) {NSNumber(unsignedInt: $0)}
+}
+
+public func !=<A: AttributeType>(lhs: A, rhs: Int32?) -> NSPredicate {
+    return predicate(lhs, "==", rhs) {NSNumber(int: $0)}
+}
+
+public func !=<A: AttributeType>(lhs: A, rhs: UInt64?) -> NSPredicate {
+    return predicate(lhs, "==", rhs) {NSNumber(unsignedLongLong: $0)}
+}
+
+public func !=<A: AttributeType>(lhs: A, rhs: Int64?) -> NSPredicate {
+    return predicate(lhs, "==", rhs) {NSNumber(longLong: $0)}
+}
+
+public func !=<A: AttributeType>(lhs: A, rhs: Int?) -> NSPredicate {
+    return predicate(lhs, "==", rhs) {NSNumber(integer: $0)}
+}
+
+public func !=<A: AttributeType>(lhs: A, rhs: Float?) -> NSPredicate {
+    return predicate(lhs, "==", rhs) {NSNumber(float: $0)}
+}
+
+public func !=<A: AttributeType>(lhs: A, rhs: Double?) -> NSPredicate {
+    return predicate(lhs, "==", rhs) {NSNumber(double: $0)}
+}
+
+/* > */
+
+public func ><A: AttributeType>(lhs: A, rhs: NSObject?) -> NSPredicate {
+    return predicate(lhs, "==", rhs) {$0}
+}
+
+public func ><A: AttributeType>(lhs: A, rhs: String?) -> NSPredicate {
+    return predicate(lhs, "==", rhs) {$0 as NSString}
+}
+
+public func ><A: AttributeType>(lhs: A, rhs: UInt8?) -> NSPredicate {
+    return predicate(lhs, "==", rhs) {NSNumber(unsignedChar: $0)}
+}
+
+public func ><A: AttributeType>(lhs: A, rhs: UInt16?) -> NSPredicate {
+    return predicate(lhs, "==", rhs) {NSNumber(unsignedShort: $0)}
+}
+
+public func ><A: AttributeType>(lhs: A, rhs: Int16?) -> NSPredicate {
+    return predicate(lhs, "==", rhs) {NSNumber(short: $0)}
+}
+
+public func ><A: AttributeType>(lhs: A, rhs: UInt32?) -> NSPredicate {
+    return predicate(lhs, "==", rhs) {NSNumber(unsignedInt: $0)}
+}
+
+public func ><A: AttributeType>(lhs: A, rhs: Int32?) -> NSPredicate {
+    return predicate(lhs, "==", rhs) {NSNumber(int: $0)}
+}
+
+public func ><A: AttributeType>(lhs: A, rhs: UInt64?) -> NSPredicate {
+    return predicate(lhs, "==", rhs) {NSNumber(unsignedLongLong: $0)}
+}
+
+public func ><A: AttributeType>(lhs: A, rhs: Int64?) -> NSPredicate {
+    return predicate(lhs, "==", rhs) {NSNumber(longLong: $0)}
+}
+
+public func ><A: AttributeType>(lhs: A, rhs: Int?) -> NSPredicate {
+    return predicate(lhs, "==", rhs) {NSNumber(integer: $0)}
+}
+
+public func ><A: AttributeType>(lhs: A, rhs: Float?) -> NSPredicate {
+    return predicate(lhs, "==", rhs) {NSNumber(float: $0)}
+}
+
+public func ><A: AttributeType>(lhs: A, rhs: Double?) -> NSPredicate {
+    return predicate(lhs, "==", rhs) {NSNumber(double: $0)}
+}
+
+/* >= */
+
+public func >=<A: AttributeType>(lhs: A, rhs: NSObject?) -> NSPredicate {
+    return predicate(lhs, "==", rhs) {$0}
+}
+
+public func >=<A: AttributeType>(lhs: A, rhs: String?) -> NSPredicate {
+    return predicate(lhs, "==", rhs) {$0 as NSString}
+}
+
+public func >=<A: AttributeType>(lhs: A, rhs: UInt8?) -> NSPredicate {
+    return predicate(lhs, "==", rhs) {NSNumber(unsignedChar: $0)}
+}
+
+public func >=<A: AttributeType>(lhs: A, rhs: UInt16?) -> NSPredicate {
+    return predicate(lhs, "==", rhs) {NSNumber(unsignedShort: $0)}
+}
+
+public func >=<A: AttributeType>(lhs: A, rhs: Int16?) -> NSPredicate {
+    return predicate(lhs, "==", rhs) {NSNumber(short: $0)}
+}
+
+public func >=<A: AttributeType>(lhs: A, rhs: UInt32?) -> NSPredicate {
+    return predicate(lhs, "==", rhs) {NSNumber(unsignedInt: $0)}
+}
+
+public func >=<A: AttributeType>(lhs: A, rhs: Int32?) -> NSPredicate {
+    return predicate(lhs, "==", rhs) {NSNumber(int: $0)}
+}
+
+public func >=<A: AttributeType>(lhs: A, rhs: UInt64?) -> NSPredicate {
+    return predicate(lhs, "==", rhs) {NSNumber(unsignedLongLong: $0)}
+}
+
+public func >=<A: AttributeType>(lhs: A, rhs: Int64?) -> NSPredicate {
+    return predicate(lhs, "==", rhs) {NSNumber(longLong: $0)}
+}
+
+public func >=<A: AttributeType>(lhs: A, rhs: Int?) -> NSPredicate {
+    return predicate(lhs, "==", rhs) {NSNumber(integer: $0)}
+}
+
+public func >=<A: AttributeType>(lhs: A, rhs: Float?) -> NSPredicate {
+    return predicate(lhs, "==", rhs) {NSNumber(float: $0)}
+}
+
+public func >=<A: AttributeType>(lhs: A, rhs: Double?) -> NSPredicate {
+    return predicate(lhs, "==", rhs) {NSNumber(double: $0)}
+}
+
+/* < */
+
+public func <<A: AttributeType>(lhs: A, rhs: NSObject?) -> NSPredicate {
+    return predicate(lhs, "==", rhs) {$0}
+}
+
+public func <<A: AttributeType>(lhs: A, rhs: String?) -> NSPredicate {
+    return predicate(lhs, "==", rhs) {$0 as NSString}
+}
+
+public func <<A: AttributeType>(lhs: A, rhs: UInt8?) -> NSPredicate {
+    return predicate(lhs, "==", rhs) {NSNumber(unsignedChar: $0)}
+}
+
+public func <<A: AttributeType>(lhs: A, rhs: UInt16?) -> NSPredicate {
+    return predicate(lhs, "==", rhs) {NSNumber(unsignedShort: $0)}
+}
+
+public func <<A: AttributeType>(lhs: A, rhs: Int16?) -> NSPredicate {
+    return predicate(lhs, "==", rhs) {NSNumber(short: $0)}
+}
+
+public func <<A: AttributeType>(lhs: A, rhs: UInt32?) -> NSPredicate {
+    return predicate(lhs, "==", rhs) {NSNumber(unsignedInt: $0)}
+}
+
+public func <<A: AttributeType>(lhs: A, rhs: Int32?) -> NSPredicate {
+    return predicate(lhs, "==", rhs) {NSNumber(int: $0)}
+}
+
+public func <<A: AttributeType>(lhs: A, rhs: UInt64?) -> NSPredicate {
+    return predicate(lhs, "==", rhs) {NSNumber(unsignedLongLong: $0)}
+}
+
+public func <<A: AttributeType>(lhs: A, rhs: Int64?) -> NSPredicate {
+    return predicate(lhs, "==", rhs) {NSNumber(longLong: $0)}
+}
+
+public func <<A: AttributeType>(lhs: A, rhs: Int?) -> NSPredicate {
+    return predicate(lhs, "==", rhs) {NSNumber(integer: $0)}
+}
+
+public func <<A: AttributeType>(lhs: A, rhs: Float?) -> NSPredicate {
+    return predicate(lhs, "==", rhs) {NSNumber(float: $0)}
+}
+
+public func <<A: AttributeType>(lhs: A, rhs: Double?) -> NSPredicate {
+    return predicate(lhs, "==", rhs) {NSNumber(double: $0)}
+}
+
+/* <= */
+
+public func <=<A: AttributeType>(lhs: A, rhs: NSObject?) -> NSPredicate {
+    return predicate(lhs, "==", rhs) {$0}
+}
+
+public func <=<A: AttributeType>(lhs: A, rhs: String?) -> NSPredicate {
+    return predicate(lhs, "==", rhs) {$0 as NSString}
+}
+
+public func <=<A: AttributeType>(lhs: A, rhs: UInt8?) -> NSPredicate {
+    return predicate(lhs, "==", rhs) {NSNumber(unsignedChar: $0)}
+}
+
+public func <=<A: AttributeType>(lhs: A, rhs: UInt16?) -> NSPredicate {
+    return predicate(lhs, "==", rhs) {NSNumber(unsignedShort: $0)}
+}
+
+public func <=<A: AttributeType>(lhs: A, rhs: Int16?) -> NSPredicate {
+    return predicate(lhs, "==", rhs) {NSNumber(short: $0)}
+}
+
+public func <=<A: AttributeType>(lhs: A, rhs: UInt32?) -> NSPredicate {
+    return predicate(lhs, "==", rhs) {NSNumber(unsignedInt: $0)}
+}
+
+public func <=<A: AttributeType>(lhs: A, rhs: Int32?) -> NSPredicate {
+    return predicate(lhs, "==", rhs) {NSNumber(int: $0)}
+}
+
+public func <=<A: AttributeType>(lhs: A, rhs: UInt64?) -> NSPredicate {
+    return predicate(lhs, "==", rhs) {NSNumber(unsignedLongLong: $0)}
+}
+
+public func <=<A: AttributeType>(lhs: A, rhs: Int64?) -> NSPredicate {
+    return predicate(lhs, "==", rhs) {NSNumber(longLong: $0)}
+}
+
+public func <=<A: AttributeType>(lhs: A, rhs: Int?) -> NSPredicate {
+    return predicate(lhs, "==", rhs) {NSNumber(integer: $0)}
+}
+
+public func <=<A: AttributeType>(lhs: A, rhs: Float?) -> NSPredicate {
+    return predicate(lhs, "==", rhs) {NSNumber(float: $0)}
+}
+
+public func <=<A: AttributeType>(lhs: A, rhs: Double?) -> NSPredicate {
+    return predicate(lhs, "==", rhs) {NSNumber(double: $0)}
 }
