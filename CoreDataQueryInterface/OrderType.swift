09b6c448b531e27431f54ddc03b9cde5beff7e74
Gregory Higley <code@revolucent.net>
2015-06-14 13:12:55 -0400

    OrderType

--- CoreDataQueryInterface/OrderType.swift
+++ CoreDataQueryInterface/OrderType.swift
@@ -9,23 +9,23 @@
 import Foundation
 
 public protocol OrderType {
-    func sortDescriptor(ascending ascending: Bool) -> NSSortDescriptor
+    func toSortDescriptor(ascending ascending: Bool) -> NSSortDescriptor
 }
 
 extension NSSortDescriptor : OrderType {
-    public func sortDescriptor(ascending ascending: Bool) -> NSSortDescriptor {
+    public func toSortDescriptor(ascending ascending: Bool) -> NSSortDescriptor {
         return self
     }
 }
 
 extension String : OrderType {
-    public func sortDescriptor(ascending ascending: Bool) -> NSSortDescriptor {
+    public func toSortDescriptor(ascending ascending: Bool) -> NSSortDescriptor {
         return NSSortDescriptor(key: self, ascending: ascending)
     }
 }
 
 extension AttributeType {
-    public func sortDescriptor(ascending ascending: Bool) -> NSSortDescriptor {
+    public func toSortDescriptor(ascending ascending: Bool) -> NSSortDescriptor {
         return NSSortDescriptor(key: String(self), ascending: ascending)
     }
 }
--- CoreDataQueryInterface/Ordering.swift
+++ CoreDataQueryInterface/Ordering.swift
@@ -9,60 +9,55 @@
 import Foundation
 
 extension QueryType {
-    public func order(descriptors: [NSSortDescriptor]) -> Self {
+    
+    func order(keys: [OrderType]) -> Self {
+        let descriptors = keys.map() { $0.toSortDescriptor(ascending: true) }
         var builder = self.builder
         builder.descriptors.extend(descriptors)
         return Self(builder: builder)
     }
-    public func order(descriptors: NSSortDescriptor...) -> Self {
-        return order(descriptors)
-    }
-    public func order(keys: [String]) -> Self {
-        let descriptors = keys.map() { NSSortDescriptor(key: $0, ascending: true) }
-        return order(descriptors)
-    }
-    public func order(keys: String...) -> Self {
+
+    func order(keys: OrderType...) -> Self {
         return order(keys)
     }
-    public func order(keys: [AttributeType]) -> Self {
-        return order(keys.map({ String($0) }))
-    }
-    public func order(keys: AttributeType...) -> Self {
-        return order(keys)
-    }
-    public func order(keys: QueryEntityType.EntityAttributeType -> [AttributeType]) -> Self {
+    
+    func order(keys: QueryEntityType.EntityAttributeType -> [OrderType]) -> Self {
         let attribute = QueryEntityType.EntityAttributeType(nil, parent: nil)
         return order(keys(attribute))
     }
-    public func order(keys: [QueryEntityType.EntityAttributeType -> AttributeType]) -> Self {
+    
+    func order(keys: [QueryEntityType.EntityAttributeType -> OrderType]) -> Self {
         let attribute = QueryEntityType.EntityAttributeType(nil, parent: nil)
-        return order(keys.map({ $0(attribute) }))
+        return order(keys.map({$0(attribute)}))
     }
-    public func order(keys: (QueryEntityType.EntityAttributeType -> AttributeType)...) -> Self {
+
+    func order(keys: (QueryEntityType.EntityAttributeType -> OrderType)...) -> Self {
         return order(keys)
     }
-    public func order(descending keys: [String]) -> Self {
-        let descriptors = keys.map() { NSSortDescriptor(key: $0, ascending: false) }
-        return order(descriptors)
+    
+    func order(descending keys: [OrderType]) -> Self {
+        let descriptors = keys.map() { $0.toSortDescriptor(ascending: false) }
+        var builder = self.builder
+        builder.descriptors.extend(descriptors)
+        return Self(builder: builder)
     }
-    public func order(descending keys: String...) -> Self {
+    
+    func order(descending keys: OrderType...) -> Self {
         return order(descending: keys)
     }
-    public func order(descending keys: [AttributeType]) -> Self {
-        return order(descending: keys.map({ String($0) }))
-    }
-    public func order(descending keys: AttributeType...) -> Self {
-        return order(descending: keys)
-    }
-    public func order(descending keys: QueryEntityType.EntityAttributeType -> [AttributeType]) -> Self {
+
+    func order(descending keys: QueryEntityType.EntityAttributeType -> [OrderType]) -> Self {
         let attribute = QueryEntityType.EntityAttributeType(nil, parent: nil)
         return order(descending: keys(attribute))
     }
-    public func order(descending keys: [QueryEntityType.EntityAttributeType -> AttributeType]) -> Self {
+    
+    func order(descending keys: [QueryEntityType.EntityAttributeType -> OrderType]) -> Self {
         let attribute = QueryEntityType.EntityAttributeType(nil, parent: nil)
-        return order(descending: keys.map({ $0(attribute) }))
+        return order(descending: keys.map({$0(attribute)}))
     }
-    public func order(descending keys: (QueryEntityType.EntityAttributeType -> AttributeType)...) -> Self {
+    
+    func order(descending keys: (QueryEntityType.EntityAttributeType -> OrderType)...) -> Self {
         return order(descending: keys)
     }
+    
 }
