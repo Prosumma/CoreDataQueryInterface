74c02082c251d88e5bbf7b90c9d20224b7214f54
Patrick Goley <patrick.goley@gmail.com>
2016-03-29 00:10:23 -0500

    added TypedExpressionConvertible protocol for providing associated types for KeyAttributes

---
+++ CoreDataQueryInterface/TypedExpressionConvertible.swift
@@ -0,0 +1,52 @@
+//
+//  TypedExpressionConvertible.swift
+//  CoreDataQueryInterface
+//
+//  Created by Patrick Goley on 3/28/16.
+//  Copyright © 2016 Prosumma LLC. All rights reserved.
+//
+
+import Foundation
+
+// MARK: Protocols
+
+public protocol ExpressionValueType {
+    
+    var boxedValue: AnyObject { get }
+}
+
+public protocol TypedExpressionConvertible: CustomExpressionConvertible {
+    
+    associatedtype ValueType: ExpressionValueType
+}
+
+// MARK: String
+
+extension String: ExpressionValueType {
+    
+    public var boxedValue: AnyObject {
+        
+        return self as NSString
+    }
+}
+
+public class StringAttribute: KeyAttribute, TypedExpressionConvertible {
+    
+    public typealias ValueType = String
+}
+
+// MARK: Date
+
+extension NSDate: ExpressionValueType {
+    
+    public var boxedValue: AnyObject {
+        
+        return self
+    }
+}
+
+public class DateAttribute: KeyAttribute, TypedExpressionConvertible {
+    
+    public typealias ValueType = NSDate
+}
+
