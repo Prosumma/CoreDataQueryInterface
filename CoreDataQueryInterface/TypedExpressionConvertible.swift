90461676ae067c319bdb2e7bf4eeb63c85a7ffb3
Patrick Goley <patrick.goley@gmail.com>
2016-03-29 01:03:04 -0500

    added support for more core data attribute types

--- CoreDataQueryInterface/TypedExpressionConvertible.swift
+++ CoreDataQueryInterface/TypedExpressionConvertible.swift
@@ -20,6 +20,7 @@ public protocol TypedExpressionConvertible: CustomExpressionConvertible {
     associatedtype ValueType: ExpressionValueType
 }
 
+
 // MARK: String
 
 extension String: ExpressionValueType {
@@ -35,6 +36,7 @@ public class StringAttribute: KeyAttribute, TypedExpressionConvertible {
     public typealias ValueType = String
 }
 
+
 // MARK: Date
 
 extension NSDate: ExpressionValueType {
@@ -49,4 +51,78 @@ public class DateAttribute: KeyAttribute, TypedExpressionConvertible {
     
     public typealias ValueType = NSDate
 }
+
+
+// MARK: Numbers
+
+extension Int: ExpressionValueType {
+    
+    public var boxedValue: AnyObject {
+        
+        return self as NSNumber
+    }
+}
+
+extension Float: ExpressionValueType {
+    
+    public var boxedValue: AnyObject {
+        
+        return self as NSNumber
+    }
+}
+
+extension Double: ExpressionValueType {
+    
+    public var boxedValue: AnyObject {
+        
+        return self as NSNumber
+    }
+}
+
+public class Integer16Attribute: KeyAttribute, TypedExpressionConvertible {
+    
+    public typealias ValueType = Int
+}
+
+public class Integer32Attribute: KeyAttribute, TypedExpressionConvertible {
+    
+    public typealias ValueType = Int
+}
+
+public class Integer64Attribute: KeyAttribute, TypedExpressionConvertible {
+    
+    public typealias ValueType = Int
+}
+
+public class FloatAttribute: KeyAttribute, TypedExpressionConvertible {
+    
+    public typealias ValueType = Float
+}
+
+public class Double64Attribute: KeyAttribute, TypedExpressionConvertible {
+    
+    public typealias ValueType = Double
+}
+
+public class DecimalAttribute: KeyAttribute, TypedExpressionConvertible {
+    
+    public typealias ValueType = Float
+}
+
+
+// MARK: Data
+
+extension NSData: ExpressionValueType {
+    
+    public var boxedValue: AnyObject {
+        
+        return self
+    }
+}
+
+public class DataAttribute: KeyAttribute, TypedExpressionConvertible {
+    
+    public typealias ValueType = NSData
+}
+
 
