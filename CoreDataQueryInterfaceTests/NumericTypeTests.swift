916e3945ef22e21f483bcb396c3a0fd33c7fd90a
Gregory Higley <code@revolucent.net>
2016-10-04 14:48:34 -0400

    All numeric tests now pass

--- CoreDataQueryInterfaceTests/NumericTypeTests.swift
+++ CoreDataQueryInterfaceTests/NumericTypeTests.swift
@@ -38,17 +38,17 @@ class NumericTypeTests: BaseTestCase {
         XCTAssert(resultCount == 1)
     }
     
-//    func testInt16ValueComparison() {
-//        let int: Int16 = 32767
-//        let resultCount = try! managedObjectContext.from(TestEntity.self).filter({ $0.integer16 == int }).count()
-//        XCTAssert(resultCount == 1)
-//    }
+    func testInt16ValueComparison() {
+        let int: Int16 = 32767
+        let resultCount = try! managedObjectContext.from(TestEntity.self).filter({ $0.integer16 == int }).count()
+        XCTAssert(resultCount == 1)
+    }
     
-//    func testUInt16ValueComparison() {
-//        let int: UInt16 = 32767
-//        let resultCount = try! managedObjectContext.from(TestEntity.self).filter({ $0.integer16 == int }).count()
-//        XCTAssert(resultCount == 1)
-//    }
+    func testUInt16ValueComparison() {
+        let int: UInt16 = 32767
+        let resultCount = try! managedObjectContext.from(TestEntity.self).filter({ $0.integer16 == int }).count()
+        XCTAssert(resultCount == 1)
+    }
     
     func testUInt32ValueComparison() {
         let int: UInt32 = 32767
@@ -67,17 +67,17 @@ class NumericTypeTests: BaseTestCase {
         XCTAssert(overflowCount == 0)
     }
     
-//    func testInt32ValueComparison() {
-//        let integer: Int32 = Int32.max
-//        let resultCount = try! managedObjectContext.from(TestEntity.self).filter({ $0.integer32 == integer }).count()
-//        XCTAssert(resultCount == 1)
-//    }
+    func testInt32ValueComparison() {
+        let integer: Int32 = Int32.max
+        let resultCount = try! managedObjectContext.from(TestEntity.self).filter({ $0.integer32 == integer }).count()
+        XCTAssert(resultCount == 1)
+    }
     
-//    func testInt64ValueComparison() {
-//        let integer: Int64 = Int64.max
-//        let resultCount = try! managedObjectContext.from(TestEntity.self).filter({ $0.integer64 == integer }).count()
-//        XCTAssert(resultCount == 1)
-//    }
+    func testInt64ValueComparison() {
+        let integer: Int64 = Int64.max
+        let resultCount = try! managedObjectContext.from(TestEntity.self).filter({ $0.integer64 == integer }).count()
+        XCTAssert(resultCount == 1)
+    }
     
     func testUIntInt32ValueComparison() {
         let integer: UInt = UInt(Int32.max)
